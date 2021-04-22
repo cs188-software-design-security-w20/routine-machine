1ad38b16efa44dba7e0f5f955dfc0c36b07ecaa8
Jody Lin <jody.m.lin@gmail.com>
2021-04-22 14:09:31 -0700

    create ring progress bar

--- Routine-Machine/lib/main.dart
+++ Routine-Machine/lib/main.dart
@@ -1,6 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:routine_machine/RingProgressBar.dart';
 import 'package:routine_machine/RoutineWidget.dart';
 
-void main() {
-  RoutineWidget r = RoutineWidget();
+import './RingProgressBar.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Routine Machine',
+      home: Scaffold(
+        body: RingProgressBar(
+          currentCount: 17,
+          goalCount: 20,
+          habitType: 'monthly',
+          color: 0xFFC960FF,
+        ),
+      ),
+    );
+  }
 }
