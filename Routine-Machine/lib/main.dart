3f449b53bf3f99cb2d379727b88fb957029f4b29
Jody Lin <jody.m.lin@gmail.com>
2021-04-25 14:13:12 -0700

    Create Check-In List View and Grey Container (#18)
    * create check-in list and grey container widget
    
    * added color and default message for empty list

--- Routine-Machine/lib/main.dart
+++ Routine-Machine/lib/main.dart
@@ -3,7 +3,14 @@ import 'package:routine_machine/RingProgressBar.dart';
 import 'package:routine_machine/RoutineWidget.dart';
 
 import './RingProgressBar.dart';
+import './CheckInList.dart';
 
+// sample data to demo the check in list
+final sampleCheckIns = <DateTime>[
+  new DateTime.now(),
+  new DateTime.utc(2021, 4, 20),
+  new DateTime.utc(2020, 4, 30),
+];
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
@@ -12,11 +19,19 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Routine Machine',
       home: Scaffold(
-        body: RingProgressBar(
-          currentCount: 17,
-          goalCount: 20,
-          habitType: 'monthly',
-          color: 0xFFC960FF,
+        body: Column(
+          children: [
+            CheckInList(
+              checkIns: sampleCheckIns,
+              color: 0xFFC960FF,
+            ),
+            RingProgressBar(
+              currentCount: 17,
+              goalCount: 20,
+              habitType: 'monthly',
+              color: 0xFFC960FF,
+            ),
+          ],
         ),
       ),
     );
