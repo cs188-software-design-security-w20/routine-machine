85baafca487f81abb5b89834caf55976123b884a
Jody Lin <jody.m.lin@gmail.com>
2021-04-29 13:27:23 -0700

    add empty scaffold for pages

---
+++ Routine-Machine/lib/Views/pages/FollowerPage.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../../constants/Palette.dart' as Palette;
+import '../../BottomNavBar.dart';
+
+class FollowerPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Follower Page'),
+      bottomNavigationBar: BottomNavBar(),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Palette.primary,
+        child: Icon(Icons.add_rounded),
+        onPressed: () => {},
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
---
+++ Routine-Machine/lib/Views/pages/LoginPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class HabitDetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Log In Page'),
+    );
+  }
+}
