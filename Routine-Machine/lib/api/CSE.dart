d40dbc33dd061ec9de8cb8bce6400ef43372188b
rtspw <richard.cx.tang@gmail.com>
2021-05-01 13:54:38 -0700

    Complete CSE Scaffold

--- Routine-Machine/lib/api/CSE.dart
+++ Routine-Machine/lib/api/CSE.dart
@@ -1,67 +1,72 @@
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:steel_crypt/steel_crypt.dart' as Steel;
+import 'package:crypton/crypton.dart' as Crypton;
 
-final storage = new FlutterSecureStorage();
-const privateKeyStorageKey = 'routine-machine-private-key';
-const publicKeyStorageKey = 'routine-machine-public-key';
-const dekStorageKey = 'routine-machine-dek';
+class CSE {
+  var storage = const FlutterSecureStorage();
+  static const privateKeyStorageKey = 'routine-machine-private-key';
+  static const publicKeyStorageKey = 'routine-machine-public-key';
+  static const dekStorageKey = 'routine-machine-dek';
+  static final CSE _instance = CSE._create();
+  factory CSE() => _instance;
+  CSE._create();
 
-class KeyPair {
-  String publicKey;
-  String privateKey;
-  KeyPair({this.publicKey, this.privateKey});
-}
+  Future<String> generateDEK() async {
+    final dek = Steel.CryptKey().genFortuna(len: 32);
+    return dek;
+  }
 
-Future<String> generateSymmetricKey() async {
-  return '';
-}
+  Future<Crypton.RSAKeypair> generateNewKeyPair() async {
+    return Crypton.RSAKeypair.fromRandom(keySize: 4096);
+  }
 
-Future<KeyPair> generateNewKeyPair() async {
-  return KeyPair(publicKey: '', privateKey: '');
-}
+  Future<String> getOwnerDEK() async {
+    final containsKey = await storage.containsKey(key: dekStorageKey);
+    if (!containsKey) await generateNewKeyPair();
+    final dek = await storage.read(key: dekStorageKey);
+    return dek;
+  }
 
-Future<String> getOwnerDEK() async {
-  final containsKey = await storage.containsKey(key: dekStorageKey);
-  if (!containsKey) await generateNewKeyPair();
-  final dek = await storage.read(key: dekStorageKey);
-  return dek;
-}
+  Future<String> getOwnerPrivateKey() async {
+    final containsKey = await storage.containsKey(key: privateKeyStorageKey);
+    if (!containsKey) await generateNewKeyPair();
+    final privateKey = await storage.read(key: privateKeyStorageKey);
+    return privateKey;
+  }
 
-Future<String> getOwnerPrivateKey() async {
-  final containsKey = await storage.containsKey(key: privateKeyStorageKey);
-  if (!containsKey) await generateNewKeyPair();
-  final privateKey = await storage.read(key: privateKeyStorageKey);
-  return privateKey;
-}
+  Future<String> getOwnerPublicKey() async {
+    final containsKey = await storage.containsKey(key: publicKeyStorageKey);
+    if (!containsKey) await generateNewKeyPair();
+    final publicKey = await storage.read(key: publicKeyStorageKey);
+    return publicKey;
+  }
 
-Future<String> getOwnerPublicKey() async {
-  final containsKey = await storage.containsKey(key: publicKeyStorageKey);
-  if (!containsKey) await generateNewKeyPair();
-  final publicKey = await storage.read(key: publicKeyStorageKey);
-  return publicKey;
-}
+  Future<void> setOwnerDEK() async {
+    final dek = await generateDEK();
+    await storage.write(key: dekStorageKey, value: dek);
+  }
 
-Future<void> setNewOwnerPKPair({String publicKey, String privateKey}) async {
-  await storage.write(key: publicKeyStorageKey, value: publicKey);
-  await storage.write(key: privateKeyStorageKey, value: privateKey);
-}
+  Future<void> setOwnerPKPair({String publicKey, String privateKey}) async {
+    throw UnimplementedError();
+  }
 
-Future<String> getEncryptedDEK({String publicKey}) async {
-  final ownerDEK = await getOwnerDEK();
-  return ownerDEK;
-}
+  Future<String> getEncryptedDEK({String publicKey}) async {
+    throw UnimplementedError();
+  }
 
-Future<String> encryptText({String text}) async {
-  return '';
-}
+  Future<String> encryptText({String text}) async {
+    throw UnimplementedError();
+  }
 
-Future<String> decryptText({String dek}) async {
-  return '';
-}
+  Future<String> decryptText({String dek}) async {
+    throw UnimplementedError();
+  }
 
-Future<String> encryptOwnerDEK({String usingPublicKey}) async {
-  return '';
-}
+  Future<String> encryptOwnerDEK({String usingPublicKey}) async {
+    throw UnimplementedError();
+  }
 
-Future<String> decryptOtherDEK({String encryptedDEK}) async {
-  return '';
+  Future<String> decryptOtherDEK({String encryptedDEK}) async {
+    throw UnimplementedError();
+  }
 }
