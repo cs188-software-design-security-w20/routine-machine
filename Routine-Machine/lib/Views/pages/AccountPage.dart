51de06abfca6f8fb49d539df8838243ba081185d
Jack Zhao <jack.zjzhao@gmail.com>
2021-05-13 00:24:39 -0700

    added QR code

--- Routine-Machine/lib/Views/pages/AccountPage.dart
+++ Routine-Machine/lib/Views/pages/AccountPage.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:routine_machine/Views/components/MenuRow.dart';
+import 'package:routine_machine/constants/Constants.dart';
 import '../components/TopBackBar.dart';
 import 'package:flutter_sfsymbols/flutter_sfsymbols.dart';
 import 'package:routine_machine/Models/UserProfile.dart';
 import 'package:routine_machine/constants/Palette.dart' as Palette;
+import 'package:qr_flutter/qr_flutter.dart';
 
 class AccountPage extends StatelessWidget {
   final UserProfile userProfile;
-
+  final String qrKey = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
   AccountPage({this.userProfile});
 
   void activeChangeNamePage(BuildContext context) {
@@ -39,9 +41,56 @@ class AccountPage extends StatelessWidget {
       context,
       MaterialPageRoute(
         builder: (context) => Scaffold(
-          appBar: TopBackBar(),
-          body: Text("QR"),
-        ),
+            appBar: TopBackBar(),
+            body: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 35),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text(
+                        "Credentials",
+                        style: TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                          fontFamily: "SF Pro Text",
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Container(
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    height: MediaQuery.of(context).size.width * 0.7,
+                    // color: Colors.white,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(25.0),
+                      boxShadow: [
+                        new BoxShadow(
+                          color: Colors.black.withOpacity(0.1),
+                          blurRadius: 10.0,
+                        ),
+                      ],
+                    ),
+                    child: Center(
+                      child: QrImage(
+                        data: this.qrKey,
+                        size: 0.5 * MediaQuery.of(context).size.width,
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 35,
+                  ),
+                  Text(
+                      "This is your secret key, scan with your second device. Please do not show this code to anyone else."),
+                ],
+              ),
+            )),
       ),
     );
   }
@@ -129,7 +178,7 @@ class AccountPage extends StatelessWidget {
                           new Icon(Icons.exit_to_app),
                         ],
                       ),
-                    )
+                    ),
                   ],
                 ),
               ],
