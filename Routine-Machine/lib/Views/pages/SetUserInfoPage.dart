149b9c068af5f83c835d095aff58cc494875de62
Jody Lin <jody.m.lin@gmail.com>
2021-05-24 12:17:07 -0700

    wrap in safe area view

--- Routine-Machine/lib/Views/pages/SetUserInfoPage.dart
+++ Routine-Machine/lib/Views/pages/SetUserInfoPage.dart
@@ -16,108 +16,110 @@ class SetUserInfoPage extends StatelessWidget {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
-      body: Container(
-        padding: EdgeInsets.all(32),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            RichText(
-              textAlign: TextAlign.start,
-              text: TextSpan(style: Constants.kLargeTitleStyle, children: [
-                TextSpan(
-                    text: 'Welcome\n',
-                    style: TextStyle(color: Palette.primary)),
-                TextSpan(text: ' to Routine Machine'),
-              ]),
-            ),
-            Padding(
-              padding: EdgeInsets.symmetric(vertical: 8),
-              child: Text(
-                'Before we begin, let\'s set up your profile',
-                style: Constants.kSubtitleStyle,
+      body: SafeArea(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              RichText(
+                textAlign: TextAlign.start,
+                text: TextSpan(style: Constants.kLargeTitleStyle, children: [
+                  TextSpan(
+                      text: 'Welcome\n',
+                      style: TextStyle(color: Palette.primary)),
+                  TextSpan(text: ' to Routine Machine'),
+                ]),
               ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'Username',
-                style: Constants.kTitle3Style,
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 8),
+                child: Text(
+                  'Before we begin, let\'s set up your profile',
+                  style: Constants.kSubtitleStyle,
+                ),
               ),
-            ),
-            TextField(
-              // username
-              style: Constants.kBodyLabelStyle,
-              controller: _usernameController,
-              decoration: InputDecoration(hintText: 'Enter a username'),
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp("[_\$a-zA-Z0-9]"))
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'First Name',
-                style: Constants.kTitle3Style,
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  'Username',
+                  style: Constants.kTitle3Style,
+                ),
               ),
-            ),
-            TextField(
-              style: Constants.kBodyLabelStyle,
-              controller: _firstNameController,
-              decoration: InputDecoration(hintText: 'Enter your first name'),
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.only(top: 20),
-              child: Text(
-                'Last Name',
-                style: Constants.kTitle3Style,
-              ),
-            ),
-            TextField(
-              style: Constants.kBodyLabelStyle,
-              controller: _lastNameController,
-              decoration: InputDecoration(hintText: 'Enter your last name'),
-              inputFormatters: [
-                FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(8),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  TextButton(
-                    onPressed: () {
-                      _setUserProfile(); // TODO: implement this
-                      Navigator.pushReplacement(
-                        context,
-                        // TODO: pass stuff to home page here
-                        FadePageRoute(builder: (context) => HomePage()),
-                      );
-                    },
-                    child: Row(
-                      children: [
-                        Text(
-                          'Begin',
-                          style: TextStyle(
-                            color: Palette.primary,
-                            fontFamily: 'SF Pro Text',
-                            fontSize: 22,
-                          ),
-                        ),
-                        Icon(
-                          Icons.arrow_forward_rounded,
-                          color: Palette.primary,
-                        ),
-                      ],
-                    ),
-                  )
+              TextField(
+                // username
+                style: Constants.kBodyLabelStyle,
+                controller: _usernameController,
+                decoration: InputDecoration(hintText: 'Enter a username'),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp("[_\$a-zA-Z0-9]"))
                 ],
               ),
-            ),
-          ],
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  'First Name',
+                  style: Constants.kTitle3Style,
+                ),
+              ),
+              TextField(
+                style: Constants.kBodyLabelStyle,
+                controller: _firstNameController,
+                decoration: InputDecoration(hintText: 'Enter your first name'),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 20),
+                child: Text(
+                  'Last Name',
+                  style: Constants.kTitle3Style,
+                ),
+              ),
+              TextField(
+                style: Constants.kBodyLabelStyle,
+                controller: _lastNameController,
+                decoration: InputDecoration(hintText: 'Enter your last name'),
+                inputFormatters: [
+                  FilteringTextInputFormatter.allow(RegExp("[a-zA-Z]"))
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(8),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    TextButton(
+                      onPressed: () {
+                        _setUserProfile(); // TODO: implement this
+                        Navigator.pushReplacement(
+                          context,
+                          // TODO: pass stuff to home page here
+                          FadePageRoute(builder: (context) => HomePage()),
+                        );
+                      },
+                      child: Row(
+                        children: [
+                          Text(
+                            'Begin',
+                            style: TextStyle(
+                              color: Palette.primary,
+                              fontFamily: 'SF Pro Text',
+                              fontSize: 22,
+                            ),
+                          ),
+                          Icon(
+                            Icons.arrow_forward_rounded,
+                            color: Palette.primary,
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
