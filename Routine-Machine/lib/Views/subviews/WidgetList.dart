f45993148af7b0f02eb7781505dac87a0b305fba
Jody Lin <jody.m.lin@gmail.com>
2021-05-25 11:05:18 -0700

    default message on empty list

--- Routine-Machine/lib/Views/subviews/WidgetList.dart
+++ Routine-Machine/lib/Views/subviews/WidgetList.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:routine_machine/Models/WidgetData.dart';
 import '../components/SmallWidgetView.dart';
+import '../../constants/Constants.dart' as Constants;
 
 class WidgetList extends StatelessWidget {
   final List<WidgetData> widgetList;
@@ -16,27 +17,37 @@ class WidgetList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: GridView.count(
-        padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-        crossAxisSpacing: 16,
-        mainAxisSpacing: 16,
-        crossAxisCount:
-            MediaQuery.of(context).orientation == Orientation.landscape ? 4 : 2,
-        childAspectRatio: 0.73,
-        children: widgetList
-            .asMap()
-            .entries
-            .map(
-              (widget) => SmallWidgetView(
-                key: ObjectKey(widget),
-                data: widget.value,
-                index: widget.key,
-                removeWidget: removeWidget,
-                updateWidget: updateWidget,
-              ),
+      child: widgetList.isNotEmpty
+          ? GridView.count(
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              crossAxisCount:
+                  MediaQuery.of(context).orientation == Orientation.landscape
+                      ? 4
+                      : 2,
+              childAspectRatio: 0.73,
+              children: widgetList
+                  .asMap()
+                  .entries
+                  .map(
+                    (widget) => SmallWidgetView(
+                      key: ObjectKey(widget),
+                      data: widget.value,
+                      index: widget.key,
+                      removeWidget: removeWidget,
+                      updateWidget: updateWidget,
+                    ),
+                  )
+                  .toList(),
             )
-            .toList(),
-      ),
+          : Center(
+              child: Text(
+                'Click the + to start\n tracking your habits :)',
+                style: Constants.kUnselectedTitleStyle,
+                textAlign: TextAlign.center,
+              ),
+            ),
     );
   }
 }
