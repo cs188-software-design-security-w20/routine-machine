25ce6cd16fea4107494815a77f6e63d3f69b80c9
Seoyoon <seoyoon.jin221@gmail.com>
2021-05-26 21:43:22 -0700

    bug fix to require either one of the user ids to be equal

--- Server/src/util/validate-id.ts
+++ Server/src/util/validate-id.ts
@@ -12,5 +12,5 @@ export const requireEqual = async (userId: any, tokenUserId: string) => {
  */
 export const requireOneEqual = async (userId1: any, userId2: any, tokenUserId: string) => {
   // eslint-disable-next-line @typescript-eslint/no-throw-literal
-  if (userId1 !== tokenUserId || userId2 !== tokenUserId) throw { message: 'user is not authorized to call this endpoint', name: 'UserUnauthorizedError' };
+  if (userId1 !== tokenUserId && userId2 !== tokenUserId) throw { message: 'user is not authorized to call this endpoint', name: 'UserUnauthorizedError' };
 };
