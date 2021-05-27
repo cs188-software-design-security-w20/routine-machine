458ed53975dfba94c0a742480d215e27e439f9f4
Seoyoon <seoyoon.jin221@gmail.com>
2021-05-26 21:36:37 -0700

    added id validation to all routers

--- Server/src/router/challenge.ts
+++ Server/src/router/challenge.ts
@@ -2,8 +2,8 @@ import { Router } from 'express';
 
 const challengeRouter = Router();
 
-challengeRouter.get('/', function(req, res) {
-	res.send("Y6Q52IlJTBSljwDpV87R7SBBFTTdllySeX3lK2vB7k8.FldA24w4Z9cOStZ-PvdUB3KobGeV-DTQm-V90-PIwlY")
+challengeRouter.get('/', (req, res) => {
+  res.send('Y6Q52IlJTBSljwDpV87R7SBBFTTdllySeX3lK2vB7k8.FldA24w4Z9cOStZ-PvdUB3KobGeV-DTQm-V90-PIwlY');
 });
 
 export default challengeRouter;
